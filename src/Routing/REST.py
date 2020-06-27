38aba63ba262d4782709aca4619f2c929e89ac97
Marcus Kertesz <ModernMAK@Hotmail.com>
2020-06-27 02:05:03 -0700

    Crashing w/ Stack Overflow?
    Something is going really wrong working, will test in another environment to see if the problem is consistant

--- src/Routing/REST.py
+++ src/Routing/REST.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Any
+from io import StringIO
+from typing import Dict, List, Any, Tuple, Union
 import configparser
 from litespeed import route
 import dicttoxml
@@ -29,29 +30,47 @@ def __convert_to_dict_list(info_list: List[BaseModel]) -> List[Dict[str, Any]]:
 
 
 def __get_request_format(request):
-    return request['GET'].get('format', 'html')
+    get_args = request['GET']
+    raw_request_format = get_args.get('format', 'html-json')
+    temp = raw_request_format.split('-')
+    base_format = None
+    sub_format = None
+    if len(temp) > 0:
+        base_format = temp[0].lower()
+    if len(temp) > 1:
+        sub_format = temp[1].lower()
+
+    return base_format, sub_format
 
 
-def serve_rest(data, request_format: str):
-    request_format = request_format.lower()
+def format_data(data, request_format: str):
     if not isinstance(data, dict):
         data = dict(data)
 
     if request_format == 'json':
-        return data
+        return json.dumps(data)
     elif request_format == 'xml':
         return dicttoxml.dicttoxml(data)
-    # elif request_format == 'ini': ~ Why would anyone need this? Nobody would, but somebody issued a challenge and i gave up on said challenge
-    #     config = configparser.SafeConfigParser()
-    #     config.read_dict(data)
-    #     return None, 200, None
-    elif request_format == 'html':
-        body = json.dumps(data, indent=4)
-        body = body.replace('\n', '<br>')
-        body = body.replace(' ', '&nbsp;')
-        return body, 200, None
+    elif request_format == 'ini':  # ~ Why would anyone need this? Nobody would, but somebody issued a challenge and i gave up on said challenge
+        config = configparser.ConfigParser()
+        config.read_dict({'ROOT': data})
+        with StringIO() as stream:
+            config.write(stream)
+            return stream.getvalue()
     else:
-        return None, 400, None
+        return None
+
+
+def serve_rest(data, request_format: Tuple[Union[str, None], Union[str, None]]):
+    if request_format[0] == 'html':
+        body = format_data(data, request_format[1])
+        if body:
+            body = body.replace('\n', '<br>')
+            body = body.replace(' ', '&nbsp;')
+            return body, 200, None
+    else:
+        return format_data(data, request_format[0])
+    return None, 400, None
 
 
 def rest_image_get(request, tag_id):
