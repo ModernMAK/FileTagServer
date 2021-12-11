9e112b71c0c5685c81d1ad13ec0684c8df84a257
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-12-11 13:32:13 -0800

    Enabled serving static/img files

--- src/FileTagServer/WEB/static.py
+++ src/FileTagServer/WEB/static.py
@@ -7,4 +7,4 @@ def mount(app: FastAPI, static_root: str = None):
     static_root = static_root or "../static"
     app.mount("/js", StaticFiles(directory=join(static_root, "js")), name="JavaScript")
     app.mount("/css", StaticFiles(directory=join(static_root, "css")), name="Cascading Style Sheet")
-    # app.mount("/img", StaticFiles(directory=join(static_root, "img")), name="Images")
+    app.mount("/img", StaticFiles(directory=join(static_root, "img")), name="Images")
