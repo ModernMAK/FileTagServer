683811294b260256cb18aa16b3670bb465641c94
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-12-11 13:53:51 -0800

    Refactor serve_streamable
    Responses are throttled at 64MiB
    
    Files are streamed (read in) at 1 MiB (StreamingResponse serves them under the hood, so how fast this is, I can't say)
    
    We now properly parse 3/4 possible byte_range requests (we don't support multi-range requests currently)
    
    parse_byte_range handles parsing the string (or lack of) into a start/end byte pair.
    handle_file_byte_range parses the start/end byte pair into a proper pair, replacing the None values with the appropriate value (per range specification).
    
    chunk_generator became file_byte_generator to keep file open/close operations in the same function
    
    limit_byte_range restricts the size of the byte_range to be no greater than the limit_size
    
    lastly, a new FileStreamingResponse easily wraps a file into a StreamingResponse (using the file path instead of a file handle)

--- src/FileTagServer/WEB/common.py
+++ src/FileTagServer/WEB/common.py
@@ -1,5 +1,6 @@
 import mimetypes
 import os
+from typing import Optional, Tuple
 
 from fastapi import FastAPI
 from fastapi.datastructures import Default
@@ -27,52 +28,87 @@ def create_renderer(**kwargs):
 Kibi = 1024
 Mebi = Kibi * Kibi
 Gibi = Mebi * Kibi
-BYTES_PER_RESPONSE = Gibi
+BYTES_PER_RESPONSE = 64 * Mebi
 BYTES_PER_CHUNK = Mebi
 
 
-def chunk_generator_from_stream(stream, chunk_size, start, size):
-    bytes_read = 0
-    stream.seek(start)
-
-    while bytes_read < size:
-        bytes_to_read = min(chunk_size,
-                            size - bytes_read)
-        yield stream.read(bytes_to_read)
-        bytes_read = bytes_read + bytes_to_read
-
-    stream.close()
+def parse_byte_range(byte_range: Optional[str]) -> Tuple[Optional[int], Optional[int]]:
+    if not byte_range:
+        return None, None
+    range_type, ranges = byte_range.split("=")
+    ranges = ranges.split(",")
+    parts = ranges[0].split("-")
+    start = int(parts[0]) if len(parts[0]) > 0 else None
+    end = int(parts[1]) if (len(parts) > 1 and len(parts[1]) > 0) else None
+    return start, end
 
 
-def serve_streamable(path: str, range: str = None):
-    if range is None:
-        return FileResponse(path)
+def file_byte_generator(path: str, start_byte: int, end_byte: int, chunk_size: int):
+    byte_range = end_byte - start_byte
+    read_size = byte_range + 1
+    with open(path, "rb") as stream:
+        bytes_read = 0
+        stream.seek(start_byte)
+        while bytes_read < read_size:
+            bytes_to_read = min(chunk_size, read_size - bytes_read)
+            yield stream.read(bytes_to_read)
+            bytes_read += bytes_to_read
 
-    size = os.stat(path).st_size
-    # !!! The file is closed in the chunk generator !!!
-    file = open(path, mode="rb")
-    start_byte_requested = int(range.split("=")[-1][:-1])
-    end_byte_planned = min(start_byte_requested + BYTES_PER_RESPONSE, size) - 1
-    # Somebody commented this correction
-    # But it'd like to point out that I ran into this EXACT issue in litespeed.
-    # The end byte is inclusive; so we subtract one from the last byte to get the proper index
 
-    # This serve also doesn't support ALL range requests, only the first
-    #   Actually, it only supports single range requests (which should be normal)
+def handle_file_byte_range(start_byte: Optional[int], end_byte: Optional[int], file_size: int) -> Tuple[int,int]:
+    if not start_byte and not end_byte:
+        start_byte = 0
+        end_byte = file_size - 1
+    elif not end_byte and start_byte:
+        end_byte = file_size - 1
+    elif not start_byte and end_byte:
+        if end_byte < 0:
+            raise Exception("End Byte is not a suffix!")
+        start_byte = (file_size - end_byte) - 1
+        end_byte = file_size - 1
+    return start_byte, end_byte
 
-    chunk_generator = chunk_generator_from_stream(
-        file,
-        chunk_size=BYTES_PER_CHUNK,
-        start=start_byte_requested,
-        size=BYTES_PER_RESPONSE
-    )
-    mime = mimetypes.guess_type(path)
-    return StreamingResponse(
-        chunk_generator,
-        headers={
-            "Accept-Ranges": "bytes",
-            "Content-Range": f"bytes {start_byte_requested}-{end_byte_planned}/{size}",
-            "Content-Type": mime[0]
-        },
-        status_code=206
-    )
+
+def limit_byte_range(start_byte: int, end_byte: int, limit_size: int = None):
+    if not limit_size:
+        return start_byte, end_byte
+    
+    range_size = end_byte - start_byte + 1
+    if range_size > limit_size:       
+        end_byte = start_byte + limit_size - 1
+        
+    return start_byte, end_byte
+
+
+class FileStreamingResponse(StreamingResponse):
+    def __init__(self, path: str, start_byte: Optional[int] = None, end_byte: Optional[int] = None, response_size: Optional[int] = None, chunk_size: Optional[int] = None, allow_unlimited_responses:bool=False):
+        # if we allow unlimited responses, keep response_size 'None' for our limit_byte_range check, if it's not None we limit, otherwise it's unlimited
+        if not allow_unlimited_responses and not response_size:
+            response_size = response_size or BYTES_PER_RESPONSE
+        chunk_size = chunk_size or BYTES_PER_CHUNK
+        file_size = os.stat(path).st_size
+        file_mime = mimetypes.guess_type(path)
+
+        start_byte, end_byte = handle_file_byte_range(start_byte, end_byte, file_size)
+        if response_size:
+            limit_byte_range(start_byte, end_byte, response_size)
+
+        streamable = file_byte_generator(path, start_byte, end_byte, chunk_size)
+
+        super().__init__(
+            streamable,
+            headers={
+                "Accept-Ranges": "bytes",
+                "Content-Range": f"bytes {start_byte}-{end_byte}/{file_size}",
+                "Content-Type": file_mime[0]
+            },
+            status_code=206
+        )
+
+
+def serve_streamable(path: str, byte_range: str = None, name: str = None):
+    if not byte_range:
+        return FileResponse(path, filename=name)
+    else:
+        start_byte, end_byte = parse_byte_range(byte_range)
+        return FileStreamingResponse(path, start_byte, end_byte)
