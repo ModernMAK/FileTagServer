a5640da02547665832c008b6d333cd5275d8722c
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-01-07 09:32:39 -0800

    webpage update

--- src/FileTagServer/WEB/index.py
+++ src/FileTagServer/WEB/index.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-from os.path import dirname
+from os.path import dirname, basename
 from typing import List, Dict, Optional
 
 from fastapi import Header
@@ -50,24 +50,25 @@ def get_file_icon(mime: Optional[str] = None) -> str:
     return get_icon(get_file_icon_name(mime))
 
 
-def build_ancestry(database: Database, name: str = "", url: str = "", path: str = "", add_self: bool = True):
+def build_breadcrumbs(database: Database, name: str = "", url: str = "", path: str = "", add_self: bool = True):
     ancestry: List[Dict] = []
     if add_self:
-        ancestry.append({'page': url, 'name': name})
+        ancestry.append({'link': url, 'text': name})
+
     while True:
         path = dirname(path)
         try:
             parent_folder = database.folder.get_folder_by_path(path)
             d = {
-                'page': reformat(folder_route, folder_id=parent_folder.id),
-                'name': parent_folder.name
+                'link': reformat(folder_route, folder_id=parent_folder.id),
+                'text': parent_folder.name
             }
             ancestry.append(d)
         except ApiError:
             break
-    ancestry.append({'page': root_route, 'name': '~'})
+    ancestry.append({'link': root_route, 'text': '~'})
     ancestry = ancestry[::-1]  # reverse order
-    ancestry[-1]['last'] = True
+    ancestry[-1]['current'] = True
     return ancestry
 
 
@@ -81,7 +82,7 @@ class WebJSONEncoder(json.JSONEncoder):
         return super(WebJSONEncoder, self).default(obj)
 
 
-def build_context(conv: WebConverter, name: str, description: str, ancestry: List[Dict], folders: List[Folder], files: List[File], tags: List[Tag], all_tags: List[Tag] = None):
+def build_folder_context(conv: WebConverter, name: str, description: str, ancestry: List[Dict], folders: List[Folder], files: List[File], tags: List[Tag], all_tags: List[Tag] = None):
     tag_lookup = {t.id: conv.tag(t) for t in all_tags} if all_tags else None
 
     folders = [conv.folder(f, tag_lookup) for f in folders] if folders else []
@@ -89,7 +90,7 @@ def build_context(conv: WebConverter, name: str, description: str, ancestry: Lis
     # Use lookup to avoid reconverting, otherwise convert
     tags = ([conv.tag(t) for t in tags] if not tag_lookup else [tag_lookup[t.id] for t in tags]) if tags else []
     files_json = Util.json(files, encoder=WebJSONEncoder, exclude_unset=True, exclude_none=True)  # [f.json() for f in files]
-    folders_json = Util.json(folders, encoder=WebJSONEncoder, exclude_unset=True, exclude_none=True) # [f.json() for f in folders]
+    folders_json = Util.json(folders, encoder=WebJSONEncoder, exclude_unset=True, exclude_none=True)  # [f.json() for f in folders]
 
     files = Util.dict(files)
     for i, f in enumerate(files):
@@ -102,7 +103,7 @@ def build_context(conv: WebConverter, name: str, description: str, ancestry: Lis
     tags = Util.dict(tags)
 
     return {
-        'ancestry': ancestry,
+        'breadcrumbs': ancestry,
         'name': name,
         'desc': description,
         'files': files,
@@ -114,20 +115,41 @@ def build_context(conv: WebConverter, name: str, description: str, ancestry: Lis
     }
 
 
+def build_file_context(conv: WebConverter, name: str, ancestry: List[Dict], file: File, all_tags: List[Tag] = None):
+    tag_lookup = {t.id: conv.tag(t) for t in all_tags} if all_tags else None
+
+    file = conv.file(file, tag_lookup) if file else None
+    # Use lookup to avoid reconverting, otherwise convert
+    # tags = ([conv.tag(t) for t in tags] if not tag_lookup else [tag_lookup[t.id] for t in tags]) if tags else []
+
+    file = Util.dict(file)
+    # folders = Util.dict(folders)
+    # tags = Util.dict(tags)
+
+    return {
+        'breadcrumbs': ancestry,
+        'name': name,
+        # 'desc': description,
+        'file': file,
+        # 'folders': folders,
+        # 'tags': tags,
+    }
+
+
 def add_routes(app: WebsiteApp):
-    def _folder(context: Dict, list_view: bool = True):
+    def __folder(context: Dict, list_view: bool = True):
         template_name = "list" if list_view else "table"
         path = app.local_pathing.html / f"folder/{template_name}.html"
         html = app.renderer.render_path(path, **context)
         return HTMLResponse(html)
 
     @app.get(root_route)
-    def root_folder():
+    def _root_folder():
         folders: List[Folder] = app.database.folder.get_root_folders()
-        ancestry = build_ancestry(app.database, add_self=False)  # Builds root only
+        ancestry = build_breadcrumbs(app.database, add_self=False)  # Builds root only
         files = None  # get_orphaned_files()
         tags = None
-        context = build_context(app.webconv, "Root", None, ancestry, folders, files, tags)
+        context = build_folder_context(app.webconv, "Root", None, ancestry, folders, files, tags)
         if app.database.file.has_orphaned_files():
             orphaned_folder = {
                 'name': 'Orphaned Files',
@@ -136,10 +158,10 @@ def add_routes(app: WebsiteApp):
                 'page': orphaned_files_route
             }
             context['folders'].append(orphaned_folder)
-        return _folder(context)
+        return __folder(context)
 
     @app.get(folder_route)
-    def sub_folder(folder_id: int):
+    def _sub_folder(folder_id: int):
         folder = app.database.folder.get_folder(folder_id)
         subfolders: List[Folder] = app.database.folder.get_folders(folder.folders)
         files = app.database.file.get_files(folder.files)
@@ -148,12 +170,12 @@ def add_routes(app: WebsiteApp):
         tag_lookup = app.database.tag.get_tags(all_tags)
         tags = [tag_lookup[tag_id] for tag_id in folder.tags] if folder.tags else None
 
-        ancestry = build_ancestry(app.database, folder.name, reformat(folder_route, folder_id=folder_id), folder.path)
-        context = build_context(app.webconv, folder.name, folder.description, ancestry, subfolders, files, tags, tag_lookup)
-        return _folder(context)
+        ancestry = build_breadcrumbs(app.database, folder.name, reformat(folder_route, folder_id=folder_id), folder.path)
+        context = build_folder_context(app.webconv, folder.name, folder.description, ancestry, subfolders, files, tags, tag_lookup)
+        return __folder(context)
 
     @app.get(orphaned_files_route)
-    def orphaned_file_folder():
+    def _orphaned_file_folder():
         folder = None
         subfolders = None
         files = app.database.file.get_orphaned_files()
@@ -162,16 +184,26 @@ def add_routes(app: WebsiteApp):
         tag_lookup = app.database.tag.get_tags(all_tags)
         tags = None
 
-        ancestry = build_ancestry(app.database, "Orphaned Files", orphaned_files_route)
-        context = build_context(app.webconv, "Orphaned Files", "Files with no parent folder.", ancestry, subfolders, files, tags, tag_lookup)
-        return _folder(context)
+        ancestry = build_breadcrumbs(app.database, "Orphaned Files", orphaned_files_route)
+        context = build_folder_context(app.webconv, "Orphaned Files", "Files with no parent folder.", ancestry, subfolders, files, tags, tag_lookup)
+        return __folder(context)
+
+    def __file(context: Dict):#, list_view: bool = True):
+        template_name = "page"# "list" if list_view else "table"
+        path = app.local_pathing.html / f"file/{template_name}.html"
+        html = app.renderer.render_path(path, **context)
+        return HTMLResponse(html)
 
     @app.get(file_route)
-    def file(file_id: int, range: Optional[str] = Header(None)):
-        return file_preview(file_id, range)
+    def _file(file_id: int):
+        file = app.database.file.get_file(file_id)
+        tag_lookup = app.database.tag.get_tags(file.tags or [])
+        ancestry = build_breadcrumbs(app.database, file.name, reformat(file_route, file_id=file_id), file.path)
+        context = build_file_context(app.webconv, file.name, ancestry, file, tag_lookup)
+        return __file(context)
 
     @app.get(file_preview_route)
-    def file_preview(file_id: int, range: Optional[str] = Header(None)):
+    def _file_preview(file_id: int, range: Optional[str] = Header(None)):
         file = app.database.file.get_file(file_id)
         if content.supports_preview(file.mime):
             preview_path = app.previews.get_preview_path(file)
